6a80b4dc0de9ab33ea49f20de7cd993dc33a8e84
theanuragshukla <tachyonanurag@gmail.com>
2023-09-15 20:40:36 +0530

    ⚡added /auth

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const http = require("http").Server(app);
-const axios = require('axios').default
-const contest = require('./models/contest.js')
 
 const db = require("./utils/database.js");
 const userRouter = require("./routes/users");
-const { performance } = require("perf_hooks");
+const authRouter = require("./routes/auth");
 
 const port = process.env.PORT || 8000;
-const baseURL = 'https://leetcode.com/contest/api/ranking';
+
+app.use(cors({
+    origin:"*",
+}))
 
 app.use(express.json());
 app.use(
@@ -18,49 +20,6 @@ app.use(
         extended: true,
     })
 );
-
-const getContestName = (number, isBiweekly) => `${isBiweekly ? "biweekly":"weekly"}-contest-${number}`
-
-
-const main = async (number, isBiweekly) => {
-    const client = axios.create({
-        baseURL:`${baseURL}/${getContestName(number, isBiweekly)}`
-    })
-    const tmp = await client.get('/', {
-        params:{
-            pagination:1,
-            region:"global"
-        }
-    } )
-    const {user_num} = tmp.data
-    const users = 0;
-    let page = 1
-    while(users<user_num){
-        const startTime = performance.now()
-        try {
-            const response = await client.get("/", {
-                params:{
-                    pagination:page++,
-                    region:"global"
-                }
-            })
-            const data = response.data.total_rank.map(({username, rank, score}) => ({username, score, rank}))
-            const fetchDelay = performance.now()
-            try {
-            const res = await contest.insertMany(data)
-            } catch (error) {
-                console.log(error)
-            }
-            const uploadDelay = performance.now()
-            console.log(page, fetchDelay - startTime, uploadDelay - fetchDelay)
-
-        } catch (error) {
-            console.error(`Error fetching data for Page ${page}: ${error.message}`);
-            break;
-        }
-    }
-}
-
 
 app.get("/", (_, res) => {
     res.json({
@@ -71,10 +30,8 @@ app.get("/", (_, res) => {
 });
 
 app.use("/users", userRouter);
+app.use('/auth', authRouter)
 
 const server = http.listen(port, () => {
     console.log(`running on port ${port}`);
-    //setTimeout(()=>{
-     //   main(112, true)
- //   }, 5000)
 });
--- server/utils/scrapeContest.js
+++ server/utils/scrapeContest.js
@@ -1,3 +1,49 @@
+const { performance } = require("perf_hooks");
+
+const baseURL = 'https://leetcode.com/contest/api/ranking';
+const axios = require('axios').default
+const contest = require('./models/contest.js')
+
+const getContestName = (number, isBiweekly) => `${isBiweekly ? "biweekly":"weekly"}-contest-${number}`
+
+const main = async (number, isBiweekly) => {
+    const client = axios.create({
+        baseURL: `${baseURL}/${getContestName(number, isBiweekly)}`
+    })
+    const tmp = await client.get('/', {
+        params: {
+            pagination: 1,
+            region: "global"
+        }
+    })
+    const { user_num } = tmp.data
+    const users = 0;
+    let page = 1
+    while (users < user_num) {
+        const startTime = performance.now()
+        try {
+            const response = await client.get("/", {
+                params: {
+                    pagination: page++,
+                    region: "global"
+                }
+            })
+            const data = response.data.total_rank.map(({ username, rank, score }) => ({ username, score, rank }))
+            const fetchDelay = performance.now()
+            try {
+                const res = await contest.insertMany(data)
+            } catch (error) {
+                console.log(error)
+            }
+            const uploadDelay = performance.now()
+            console.log(page, fetchDelay - startTime, uploadDelay - fetchDelay)
+
+        } catch (error) {
+            console.error(`Error fetching data for Page ${page}: ${error.message}`);
+            break;
+        }
+    }
+}
 
 
 
